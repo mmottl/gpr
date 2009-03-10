89149955f75285f087376ec84f120f0a8087db00
Markus Mottl <markus.mottl@gmail.com>
2009-03-09 22:39:00 -0400

    Split up test suite

---
+++ test/test_kernels.ml
@@ -0,0 +1,32 @@
+open Lacaml.Impl.D
+
+open Gpr
+
+module Const = struct
+  include Fitc.Make_deriv (Cov_const)
+
+  let params = { Cov_const.Params.log_theta = log 1. }
+  let kernel = Cov_const.Eval.Kernel.create params
+end
+
+module Lin_one = struct
+  include Fitc.Make_deriv (Cov_lin_one)
+
+  let params = { Cov_lin_one.Params.log_theta = log 1. }
+  let kernel = Cov_lin_one.Eval.Kernel.create params
+end
+
+module Lin_ard = struct
+  include Fitc.Make_deriv (Cov_lin_ard)
+
+  (* Any other value with ARD is useless for graphical demonstration *)
+  let params = { Cov_lin_ard.Params.log_ells = Vec.make 1 0. }
+  let kernel = Cov_lin_ard.Eval.Kernel.create params
+end
+
+module SE_iso = struct
+  include Fitc.Make_deriv (Cov_se_iso)
+
+  let params = { Cov_se_iso.Params.log_ell = log 1.; log_sf = log 1. }
+  let kernel = Cov_se_iso.Eval.Kernel.create params
+end
