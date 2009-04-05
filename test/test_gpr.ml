2124d9cf4aaae65914e4cfb9e1152fa7b9724e7c
Markus Mottl <markus.mottl@gmail.com>
2009-04-05 12:37:27 -0400

    Added testing for derivatives of inducing points

--- test/test_gpr.ml
+++ test/test_gpr.ml
@@ -9,10 +9,11 @@ open Utils
 open Test_kernels.SE_iso
 open Gen_data
 
+(*
 let main () =
   let sigma2 = noise_sigma2 in
 
-  let epsilon = 10e-6 in
+  let epsilon = 1e-6 in
 
   let module Eval = FITC.Eval in
   let module Deriv = FITC.Deriv in
@@ -66,5 +67,47 @@ let main () =
   printf "log evidence: %f\n%!" f1;
   printf "dlog_evidence: %f\n%!" deriv;
   printf "dfinite:   %f\n%!" ((f2 -. f1) /. epsilon)
+*)
+
+let main () =
+  Lacaml.Io.Context.set_dim_defaults (Some (Context.create 5));
+
+  let sigma2 = noise_sigma2 in
+
+  let epsilon = 1e-6 in
+
+  let module Eval = FITC.Eval in
+  let module Deriv = FITC.Deriv in
+
+  Utils.print_mat "inducing_inputs" inducing_inputs;
+  let run () =
+    let eval_prep_inducing = Eval.Inducing.Prepared.calc inducing_inputs in
+    let deriv_prep_inducing = Deriv.Inducing.Prepared.calc eval_prep_inducing in
+    let inducing = Deriv.Inducing.calc kernel deriv_prep_inducing in
+    let eval_prep_inputs =
+      Eval.Inputs.Prepared.calc eval_prep_inducing training_inputs
+    in
+    let deriv_prep_inputs =
+      Deriv.Inputs.Prepared.calc deriv_prep_inducing eval_prep_inputs
+    in
+    let inputs = Deriv.Inputs.calc inducing deriv_prep_inputs in
+    let model = Deriv.Model.calc ~sigma2 inputs in
+
+    let hyper_model = Deriv.Model.prepare_hyper model in
+    let mev, _model_log_evidence =
+      Deriv.Model.calc_log_evidence hyper_model (`Inducing_hyper {
+        Cov_se_iso.ind = 3; dim = 1 })
+    in
+
+    let mf = Eval.Model.calc_log_evidence (Deriv.Model.calc_eval model) in
+    mev, mf
+  in
+
+  let mev, mf1 = run () in
+  inducing_inputs.{1, 3} <- inducing_inputs.{1, 3} +. epsilon;
+  let _, mf2 = run () in
+
+  printf "mdlog_evidence: %f\n%!" mev;
+  printf "mdfinite:   %f\n%!" ((mf2 -. mf1) /. epsilon)
 
 let () = main ()
