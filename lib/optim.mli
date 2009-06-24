1647aab90d56a5290cb38b89ce15363546aef5d7
Markus Mottl <markus.mottl@gmail.com>
2009-06-24 00:06:46 -0400

    Namespace improvements

--- lib/optim.mli
+++ lib/optim.mli
@@ -2,40 +2,42 @@ open Lacaml.Impl.D
 
 open Interfaces
 
-(* Ordinary hyper parameter optimization *)
+(** Hyper parameter optimization with the GNU Scientific Library *)
+module Gsl : sig
 
-module Make (Spec : Sigs.Deriv) : sig
-  open Spec
+  (** Ordinary hyper parameter optimization *)
+  module Make (Spec : Sigs.Deriv) : sig
+    open Spec
 
-  module Solution : sig
-    type t = {
-      sigma2 : float;
-      kernel : Eval.Spec.Kernel.t;
-      coeffs : vec;
-      log_evidence : float;
-    }
+    module Solution : sig
+      type t = {
+        sigma2 : float;
+        kernel : Eval.Spec.Kernel.t;
+        coeffs : vec;
+        log_evidence : float;
+      }
+    end
+
+    val solve :
+      ?kernel : Eval.Spec.Kernel.t ->
+      ?sigma2 : float ->
+      ?inducing : Eval.Spec.Inducing.t ->
+      ?n_rand_inducing : int ->
+      inputs : Eval.Spec.Inputs.t ->
+      targets : vec ->
+      Solution.t
   end
 
-  val solve :
-    ?kernel : Eval.Spec.Kernel.t ->
-    ?sigma2 : float ->
-    ?inducing : Eval.Spec.Inducing.t ->
-    ?n_rand_inducing : int ->
-    inputs : Eval.Spec.Inputs.t ->
-    targets : vec ->
-    Solution.t
+
+  (** SPGP *)
+  module Make_SPGP
+    (Deriv : Sigs.Deriv)
+    (Spec : Specs.SPGP
+      with module Eval = Deriv.Eval.Spec
+      with module Deriv = Deriv.Deriv.Spec)
+    :
+      Sigs.SPGP
+        with module Eval = Deriv.Eval
+        with module Deriv = Deriv
+        with module SPGP.Spec = Spec
 end
-
-
-(** SPGP *)
-
-module Make_SPGP
-  (Deriv : Sigs.Deriv)
-  (Spec : Specs.SPGP
-    with module Eval = Deriv.Eval.Spec
-    with module Deriv = Deriv.Deriv.Spec)
-  :
-    Sigs.SPGP
-      with module Eval = Deriv.Eval
-      with module Deriv = Deriv
-      with module SPGP.Spec = Spec
